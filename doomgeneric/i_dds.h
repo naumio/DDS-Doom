6c5812dd36d2af71d0392b5eb3fd3a993ed6e077
naumio <joonasnaumanen@gmail.com>
2024-12-26 22:16:53 +0200

    Player input from a DDS topic
    - Implement player input reading from a DDS topic
    - Add separate Makefile for building with DDS features
    - Add ROS2 compatible IDL files
    - Add IDL generated source files
    - Add VSCode settings in .gitignore
    - Add WAD files in .gitignore

---
+++ doomgeneric/i_dds.h
@@ -0,0 +1,27 @@
+//
+// Copyright(C) 2005-2014 Simon Howard
+//
+// This program is free software; you can redistribute it and/or
+// modify it under the terms of the GNU General Public License
+// as published by the Free Software Foundation; either version 2
+// of the License, or (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// DESCRIPTION:
+//      DDS interface.
+//
+
+
+#ifndef __I_DDS__
+#define __I_DDS__
+
+void I_InitDDS(void);
+void I_ShutdownDDS(void);
+void I_UpdateDDS(void);
+
+#endif /* #ifndef __I_DDS__ */
+
